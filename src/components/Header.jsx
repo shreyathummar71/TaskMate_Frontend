bf48dfa9e0c3d3d133a5605071b0ed85e4a1eb81
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-23 16:22:01 +0200

    rough representation of Header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,45 @@
+import HeroSection from "./HeroSection";
+
 const Header = () => {
-  return <></>;
+  return (
+    <>
+      <div className="bg-gray-800 p-4 flex justify-between items-center">
+        <h1 className="text-white text-2xl font-bold">TaskMate</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a className="text-white hover:text-yellow-400" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-yellow-400" href="#">
+                Services
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-yellow-400" href="#">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-yellow-400" href="#">
+                FAQ
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-yellow-400" href="#">
+                Blogs
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <HeroSection />
+      </div>
+    </>
+  );
 };
 
 export default Header;
