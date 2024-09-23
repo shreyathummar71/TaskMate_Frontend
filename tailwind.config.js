2c3758d3e17a232ed9e8e7edd3a95b83eca4100a
Shreya <shreyathummar71@gmail.com>
2024-09-23 15:34:34 +0200

    custom classes added in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#273343", // Your primary color
+        secondary: "#F7D552", // Your secondary color
+        tertiary: "#B7BABF", // Your tertiary color
+      },
+      fontFamily: {
+        primary: ['"Inknut Antiqua"', "sans-serif"], // Your primary font
+        secondary: ['"Kalnia"', "serif"], // Your secondary font
+      },
+    },
   },
   plugins: [daisyui],
 };
