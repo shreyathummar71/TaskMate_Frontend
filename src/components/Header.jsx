e2640a51ff6298a1b1d88e1b94d6c16adc4a8906
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-23 17:09:42 +0200

    add text color and font to nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,32 +3,50 @@ import HeroSection from "./HeroSection";
 const Header = () => {
   return (
     <>
-      <div className="tertiary p-4 flex justify-between items-center">
-        <h1 className="text-white text-2xl font-bold">TaskMate</h1>
+      <div className="bg-tertiary p-4 flex justify-between items-center">
+        <h1 className=" font-primary text-2xl font-bold">
+          <span className="text-primary">Task</span>
+          <span className="text-secondary">Mate</span>
+        </h1>
         <nav>
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-6">
             <li>
-              <a className="text-white hover:text-yellow-400" href="#">
+              <a
+                className="text-primary font-primary hover:text-yellow-400"
+                href="#"
+              >
                 Home
               </a>
             </li>
             <li>
-              <a className="text-white hover:text-yellow-400" href="#">
+              <a
+                className="text-primary font-primary hover:text-yellow-400"
+                href="#"
+              >
                 Services
               </a>
             </li>
             <li>
-              <a className="text-white hover:text-yellow-400" href="#">
+              <a
+                className="text-primary font-primary hover:text-yellow-400"
+                href="#"
+              >
                 About Us
               </a>
             </li>
             <li>
-              <a className="text-white hover:text-yellow-400" href="#">
+              <a
+                className="text-primary font-primary hover:text-yellow-400"
+                href="#"
+              >
                 FAQ
               </a>
             </li>
             <li>
-              <a className="text-white hover:text-yellow-400" href="#">
+              <a
+                className="text-primary font-primary hover:text-yellow-400"
+                href="#"
+              >
                 Blogs
               </a>
             </li>
@@ -36,7 +54,7 @@ const Header = () => {
         </nav>
       </div>
       <div>
-        <HeroSection />
+        <Hero />
       </div>
     </>
   );
