72d7c2802bcd38e0c853e81fecd6fc2e893ff726
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-23 16:36:12 +0200

    add Herosection in next div of Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import HeroSection from "./HeroSection";
 const Header = () => {
   return (
     <>
-      <div className="bg-gray-800 p-4 flex justify-between items-center">
+      <div className="tertiary p-4 flex justify-between items-center">
         <h1 className="text-white text-2xl font-bold">TaskMate</h1>
         <nav>
           <ul className="flex space-x-4">
