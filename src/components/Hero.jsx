6617fe19ff664ebb7dfff34879fc093fb8c74020
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-24 12:04:20 +0200

    create a button colclick function to call signup component for customer and professional

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import SignupCust from "./customer/SignupCust";
+import SignupProf from "./professional/SignupProf";
 
 const Hero = () => {
+  // Create a state variable to control whether the customer signup component should be displayed:
+  const [showSignupCust, setShowSignupCust] = useState(false);
+
+  // Create a state variable to control whether the Professional signup component should be displayed:
+  const [showSignupProf, setShowSignupProf] = useState(false);
+
+  // Create a function to handle the SignupCust button click:
+  const handleSignupCustClick = () => {
+    setShowSignupCust(true);
+  };
+
+  // Create a function to handle the SignupProf button click:
+  const handleSignupProfClick = () => {
+    setShowSignupProf(true);
+  };
+
   return (
     <div>
       <section
@@ -19,7 +37,10 @@ const Hero = () => {
               Local Service Providers!
             </p>
             <div className="flex space-x-4 mt-5">
-              <button className="bg-tertiary border border-secondary font-secondary font-semibold text-white  py-2 px-4 rounded-2xl shadow-lg mb-6 inline-flex items-center">
+              <button
+                className="bg-tertiary border border-secondary font-secondary font-semibold text-white  py-2 px-4 rounded-xl shadow-lg mb-6 inline-flex items-center"
+                onClick={handleSignupCustClick}
+              >
                 <span class="mr-2">Get Started</span>
                 <span>
                   <img
@@ -29,11 +50,16 @@ const Hero = () => {
                   ></img>
                 </span>
               </button>
+              {showSignupCust && <SignupCust />}
             </div>
             <div className="flex space-x-4">
-              <button className="bg-tertiary border  border-secondary font-secondary font-semibold text-white  py-2 px-4 rounded-xl shadow-lg">
+              <button
+                className="bg-tertiary border  border-secondary font-secondary font-semibold text-white  py-2 px-4 rounded-xl shadow-lg"
+                onClick={setShowSignupProf}
+              >
                 Join as a Professional
               </button>
+              {showSignupProf && <SignupProf />}
             </div>
           </div>
         </div>
