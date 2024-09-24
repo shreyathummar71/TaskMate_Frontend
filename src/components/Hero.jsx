e7ea6be4cec556fd0a4e6e1d5723d2bb4736c807
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-24 11:21:19 +0200

    add arrow image in button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,18 +12,29 @@ const Hero = () => {
         <div className="absolute inset-0 opacity-50"></div>{" "}
         {/* Overlay for better text visibility */}
         <div className="relative z-10 flex items-center justify-start h-full p-5">
-          <div className="font-primary text-left text-white">
-            <h2 className="text-2xl font-semibold font-primary mb-8">
-              Find <span className="text-secondary mb-10">Trusted</span>
-              <br /> Local Service Providers!
-            </h2>
-            <div className="flex space-x-4">
-              <button className="bg-tertiary border border-secondary font-secondary text-white font-semibold py-2 px-4 rounded-xl shadow-lg mb-6">
-                Get Started &raquo;
+          <div className=" text-left text-white">
+            <p className="text-3xl font-semibold font-primary mb-4">
+              Find <span className="text-secondary">Trusted</span>
+            </p>
+            <p className="text-2xl font-semibold font-primary mb-8">
+              Local Service Providers!
+            </p>
+
+            <div className="flex space-x-4 mt-5">
+              <button className="bg-tertiary border border-secondary font-secondary font-semibold text-white  py-2 px-4 rounded-2xl shadow-lg mb-6 inline-flex items-center">
+                <span class="mr-2">Get Started</span>{" "}
+                {/* <span className="font-bold"> &raquo;</span> */}
+                <span>
+                  <img
+                    src="src/assets/images/buttonArrow.png"
+                    alt="arrowButton"
+                    width="20"
+                  ></img>
+                </span>
               </button>
             </div>
             <div className="flex space-x-4">
-              <button className="bg-tertiary border  border-secondary font-secondary text-white font-semibold py-2 px-4 rounded-xl shadow-lg">
+              <button className="bg-tertiary border  border-secondary font-secondary font-semibold text-white  py-2 px-4 rounded-xl shadow-lg">
                 Join as a Professional
               </button>
             </div>
