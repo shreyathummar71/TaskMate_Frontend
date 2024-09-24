db4472be6683e077e99de689a3cc9c02f4eb61ed
AnjaliGrover22 <anjaligrover2210@gmail.com>
2024-09-24 19:59:29 +0200

    Signup and Login Design completed , also header footer removal logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import Blogs from "./components/Blogs";
 import SignupCust from "./components/customer/SignupCust";
 import SignupProf from "./components/professional/SignupProf";
+import LoginCust from "./components/customer/LoginCust";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -26,6 +27,7 @@ const App = () => {
           <Route path="/AllCategory" element={<AllCategory />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/signupCust" element={<SignupCust />} />
+          <Route path="/login" element={<LoginCust />} />
           <Route path="/signupProf" element={<SignupProf />} />
         </Route>
       </>
