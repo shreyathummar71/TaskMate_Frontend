5fb8e93f66db2f91cc6e0519459c344e631c54ed
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-24 17:32:55 +0200

    add route for FAQ

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import Blogs from "./components/Blogs";
 import SignupCust from "./components/customer/SignupCust";
 import SignupProf from "./components/professional/SignupProf";
+import FAQ from "./pages/FAQ";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -27,6 +28,7 @@ const App = () => {
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/signupCust" element={<SignupCust />} />
           <Route path="/signupProf" element={<SignupProf />} />
+          <Route path="/faq" element={<FAQ />} />
         </Route>
       </>
     )
