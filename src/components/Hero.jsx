c0bf59190ed25865ecbecc6387b903716cf53dea
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-24 11:40:29 +0200

    apply css on Hero section text and buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,12 @@ const Hero = () => {
   return (
     <div>
       <section
-        className="relative h-screen bg-cover bg-center"
+        className="relative h-screen bg-cover bg-center p-7"
         style={{
           backgroundImage: "url('src/assets/images/hero_background.png')",
         }}
       >
-        <div className="absolute inset-0 opacity-50"></div>{" "}
-        {/* Overlay for better text visibility */}
+        <div className="absolute inset-0 opacity-50"></div>
         <div className="relative z-10 flex items-center justify-start h-full p-5">
           <div className=" text-left text-white">
             <p className="text-3xl font-semibold font-primary mb-4">
@@ -19,11 +18,9 @@ const Hero = () => {
             <p className="text-2xl font-semibold font-primary mb-8">
               Local Service Providers!
             </p>
-
             <div className="flex space-x-4 mt-5">
               <button className="bg-tertiary border border-secondary font-secondary font-semibold text-white  py-2 px-4 rounded-2xl shadow-lg mb-6 inline-flex items-center">
-                <span class="mr-2">Get Started</span>{" "}
-                {/* <span className="font-bold"> &raquo;</span> */}
+                <span class="mr-2">Get Started</span>
                 <span>
                   <img
                     src="src/assets/images/buttonArrow.png"
