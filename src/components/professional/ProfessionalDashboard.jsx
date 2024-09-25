affde3d78f8cc94f5af0509b3680a020f432b25c
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-25 12:28:38 +0200

    provide link to FAQProfessional  page

--- src/components/professional/ProfessionalDashboard.jsx
+++ src/components/professional/ProfessionalDashboard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ProfessionalDashboard = () => {
-  return <div>ProfessionalDashboard</div>;
+  return (
+    <div>
+      ProfessionalDashboard
+      <Link to="/faqProfessional" className="text-lg font-tertiary mt-2">
+        <span className="text-primary">FAQ</span>
+        <span className="text-secondary">Professional</span>
+      </Link>
+    </div>
+  );
 };
 
 export default ProfessionalDashboard;
