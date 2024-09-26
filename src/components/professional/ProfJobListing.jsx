66ef245a619da899dea3170bcff7a15aa72d8e8c
dhruvi <dhruvi.balar@gmail.com>
2024-09-26 18:58:57 +0200

    add job listing modal and now only data post in backend

--- src/components/professional/ProfJobListing.jsx
+++ src/components/professional/ProfJobListing.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import AddJob from "./AddJob"; // Import AddJob component
 
 const ProfJobListing = () => {
-  return <div>joblisting</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
+  return (
+    <div className="relative">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl text-primary font-primary">Job Listing</h1>
+        <button
+          onClick={handleOpenModal}
+          className="bg-primary text-white font-primary py-2 px-4 rounded-xl shadow-md hover:bg-secondary"
+        >
+          <i className="fas fa-plus mr-2 text-secondary"></i>
+          Add Job
+        </button>
+      </div>
+
+      {/* Modal for Adding Job */}
+      <AddJob isModalOpen={isModalOpen} handleCloseModal={handleCloseModal} />
+    </div>
+  );
 };
 
 export default ProfJobListing;
