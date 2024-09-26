d2befe83092d823047d61bcbf0c43ddd198a6914
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-26 19:17:09 +0200

    add style on form

--- src/components/customer/CustHelpCenter.jsx
+++ src/components/customer/CustHelpCenter.jsx
@@ -11,20 +11,22 @@ const CustHelpCenter = () => {
   return (
     <div className="flex flex-col">
       {!showContactForm ? (
-        <div className="help-center-container bg-gray-100 p-8">
+        <div className="help-center-container bg-tertiary p-8">
           {/* Existing help center content */}
-          <h1 className="text-3xl font-bold text-primary mb-6">Help Center</h1>
+          <h1 className="text-3xl font-bold text-primary font-primary mb-6">
+            Help Center
+          </h1>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {/* Help by Us */}
             <div className="bg-white p-6 rounded-lg shadow-md">
-              <h2 className="text-xl font-semibold text-primary mb-4">
+              <h2 className="text-xl  font-primary font-semibold text-primary mb-4">
                 Help by Us
               </h2>
-              <p className="text-gray-700">
+              <p className="font-secondary text-primary font-normal ">
                 Contact our support team for personalized assistance.
               </p>
               <button
-                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+                className="mt-2 bg-tertiary bg-opacity-50 border border-secondary text-primary px-4 py-2 rounded-xl font-primary text-sm hover:bg-secondary hover:text-white"
                 onClick={handleHelpByUSClick}
               >
                 Help by Us
