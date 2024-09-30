fdc99c4f6c701006a6cc0d77550759666d42bb9c
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-30 14:29:44 +0200

    display cards of professionals on the basis of  services

--- src/components/customer/ProfessionalsListByService.jsx
+++ src/components/customer/ProfessionalsListByService.jsx
@@ -1,8 +1,10 @@
 // ProfessionalsListByService.js
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-const PROFESSIONAL_JOBS_API_URL =
-  "https://backend-taskmate.onrender.com/newJob/professional";
+import axios from "axios";
+
+const PROFESSIONAL_BY_SERVICE_ID_URL =
+  "https://backend-taskmate.onrender.com/newJob/professionals-for-service-details/";
 
 const ProfessionalsListByService = ({ serviceId }) => {
   const [professionals, setProfessionals] = useState([]);
@@ -10,23 +12,23 @@ const ProfessionalsListByService = ({ serviceId }) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (serviceId) {
-      const fetchProfessionals = async () => {
-        try {
-          const response = await fetch(PROFESSIONAL_JOBS_API_URL);
-          if (!response.ok) {
-            throw new Error("No professionals available for this service");
-          }
-          const data = await response.json();
-          setProfessionals(data);
-          console.log(professionals); // debug to get professional data
-        } catch (err) {
-          setError(err.message);
-        } finally {
-          setLoading(false);
-        }
-      };
+    const fetchProfessionals = async () => {
+      try {
+        const response = await axios.get(
+          `${PROFESSIONAL_BY_SERVICE_ID_URL}${serviceId}`
+        );
+        setProfessionals(response.data);
+      } catch (err) {
+        setError(
+          err.response?.data?.message ||
+            "No professionals available for this service"
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    if (serviceId) {
       fetchProfessionals();
     }
   }, [serviceId]);
@@ -35,34 +37,34 @@ const ProfessionalsListByService = ({ serviceId }) => {
   if (error) return <div> {error}</div>;
 
   // Function to render stars based on average rating
-  const renderStars = (rating) => {
-    const stars = [];
-    const filledStars = Math.floor(rating); // Get the whole number part of the rating
-    const halfStar = rating % 1 !== 0; // Check if there's a half star
+  // const renderStars = (rating) => {
+  //   const stars = [];
+  //   const filledStars = Math.floor(rating); // Get the whole number part of the rating
+  //   const halfStar = rating % 1 !== 0; // Check if there's a half star
 
-    for (let i = 0; i < 5; i++) {
-      if (i < filledStars) {
-        stars.push(
-          <span key={i} className="text-yellow-500 text-2xl">
-            ★
-          </span>
-        ); // Filled star
-      } else if (i === filledStars && halfStar) {
-        stars.push(
-          <span key={i} className="text-yellow-500 text-2xl">
-            ☆
-          </span>
-        ); // Half star (optional)
-      } else {
-        stars.push(
-          <span key={i} className="text-gray-400 text-2xl">
-            ☆
-          </span>
-        ); // Empty star
-      }
-    }
-    return stars;
-  };
+  //   for (let i = 0; i < 5; i++) {
+  //     if (i < filledStars) {
+  //       stars.push(
+  //         <span key={i} className="text-yellow-500 text-2xl">
+  //           ★
+  //         </span>
+  //       ); // Filled star
+  //     } else if (i === filledStars && halfStar) {
+  //       stars.push(
+  //         <span key={i} className="text-yellow-500 text-2xl">
+  //           ☆
+  //         </span>
+  //       ); // Half star (optional)
+  //     } else {
+  //       stars.push(
+  //         <span key={i} className="text-gray-400 text-2xl">
+  //           ☆
+  //         </span>
+  //       ); // Empty star
+  //     }
+  //   }
+  //   return stars;
+  // };
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
@@ -86,14 +88,36 @@ const ProfessionalsListByService = ({ serviceId }) => {
             </div>
             <div>
               <p className="text-sm  text-left mt-2 ml-3 text-white font-secondary ">
-                work Experience : {professional.jobProfile.experience} years
+                ServiceName : {professional.serviceName}
               </p>
             </div>
             <div>
+              <p className="text-sm  text-left mt-2 ml-3 text-white font-secondary ">
+                Location: : {professional.city} , {professional.country}
+              </p>
+            </div>
+            <div>
+              <p className="text-sm  text-left mt-2 ml-3 text-white font-secondary ">
+                Charges : ${professional.chargesPerHour}/hour
+              </p>
+            </div>
+            <div>
+              <p className="text-sm  text-left mt-2 ml-3 text-white font-secondary ">
+                Working Date: : {professional.workingDate}
+              </p>
+            </div>
+            <div>
+              <p className="text-sm  text-left mt-2 ml-3 text-white font-secondary ">
+                Working Time: : {professional.workingTime.start} -{" "}
+                {professional.workingTime.end}
+              </p>
+            </div>
+
+            {/* <div>
               <p className="text-sm  text-left mt-2 ml-3 text-white font-secondary">
                 Average Rating : {renderStars(professional.averageRating)}
               </p>
-            </div>
+            </div> */}
           </div>
 
           <Link
