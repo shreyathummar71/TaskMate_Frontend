3f0742808a711607724a8d0945937fbb49a3cfc1
shital Dalavi <shital.dalavi3@gmail.com>
2024-10-04 10:37:08 +0200

    update booking functionality

--- src/components/customer/CustMyBooking.jsx
+++ src/components/customer/CustMyBooking.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import getCustomerIdFromToken from "../../utils/tokenUtils";
 import userImage from "/src/assets/images/user.png";
-import ModifyBookingModal from "./ModifyBookingModal";
+//import ModifyBookingModal from "./ModifyBookingModal";
+import BookingModal from "./BookingModal";
 
 const CustMyBooking = () => {
   const [customerId, setCustomerId] = useState(null);
