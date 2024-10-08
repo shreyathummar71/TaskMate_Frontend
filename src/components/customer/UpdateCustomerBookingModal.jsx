d37909a1f5e9b8e8cd3f427d0c7312b56c457b02
shital Dalavi <shital.dalavi3@gmail.com>
2024-10-08 16:43:39 +0200

    add alert message

--- src/components/customer/UpdateCustomerBookingModal.jsx
+++ src/components/customer/UpdateCustomerBookingModal.jsx
@@ -31,7 +31,7 @@ const UpdateCustomerBookingModal = ({
 
   const [customerBookingData, setCustomerBookingData] = useState(""); //New state for Customer booking
   const [isLoading, setIsLoading] = useState(true);
-  const [successMessage, setSuccessMessage] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
 
   function formatTime(timeString) {
     const date = new Date(timeString);
@@ -145,14 +145,17 @@ const UpdateCustomerBookingModal = ({
       if (response.ok) {
         const updatedBooking = await response.json();
         console.log("Booking updated successfully:", updatedBooking);
-        setSuccessMessage("Booking updated successfully!");
-
-        // Close the modal after 2 seconds
-        setTimeout(() => {
-          setSuccessMessage("");
-          onClose();
-        }, 2000);
         onSubmit(updatedBooking);
+        // Show success alert
+
+        // console.log(setAlertMessage("Booking confirmed successfully!"));
+
+        setAlertMessage("Booking confirmed successfully!");
+
+        // Clear alert after 3 seconds
+        setTimeout(() => {
+          setAlertMessage("");
+        }, 3000);
       } else {
         console.error("Failed to update booking");
       }
@@ -164,6 +167,11 @@ const UpdateCustomerBookingModal = ({
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-primary rounded-lg p-6 w-1/3 max-h-full overflow-y-auto">
+        {alertMessage && (
+          <div className="absolute top-4 right-4 bg-green-500 text-white p-2 rounded z-50">
+            {alertMessage}
+          </div>
+        )}
         <h2 className="text-xl font-primary text-secondary text-center mb-4">
           Book Appointment
         </h2>
@@ -378,9 +386,6 @@ const UpdateCustomerBookingModal = ({
             </button>
           </div>
         </form>
-        {successMessage && (
-          <div className="text-secondary">{successMessage}</div>
-        )}
       </div>
     </div>
   );
